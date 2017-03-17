6f949f966390535f85d96f43849ac8fe506c89a1
Osvaldo Rodríguez <orodriguez@seiza-ti.cl>
2017-03-17 17:00:56 -0300

    Se agrega verbosidad al script de espera de mysql

--- volumes/mysql-sonarqube/esperar-inicio-servicio-mysql.sh
+++ volumes/mysql-sonarqube/esperar-inicio-servicio-mysql.sh
@@ -6,12 +6,19 @@ function esperar() {
 
 	while [ $i -lt $1 ] && [[ $iniciado -ne 0 ]]; 
 	do 
-		echo Verificando estado de MySQL...;  
+		echo "Esperando que MySQL se inicie completamente... (intento $i)";  
 		(echo > /dev/tcp/localhost/3306) >/dev/null 2>&1; 
 		iniciado=$?; 
 		i=$[$i +1]; 
 		sleep 1; 
 	done;
+	
+	if [[ $iniciado -eq 0 ]]; then
+		echo "MySQL Iniciado."
+	else
+		echo "Tiempo de espera agotado. "
+	fi
+	
 
 	return $iniciado
 }
